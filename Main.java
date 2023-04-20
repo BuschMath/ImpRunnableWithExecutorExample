c0f24fcd1d578b8dfc85e5f457de567d6e9b04ab
BuschMath <busch@iowacentral.edu>
2023-04-20 13:36:38 -0500

    Added another example

--- Main.java
+++ Main.java
@@ -1,3 +1,6 @@
+/* Implements two different approaches to implementing 
+ *      Runnable.
+ */
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -9,8 +12,21 @@ public class Main {
         // Submit two tasks to the thread pool
         executor.submit(new MyTask(1));
         executor.submit(new MyTask(2));
+
+        Runnable r3 = TaskTwo(3);
+        Runnable r4 = TaskTwo(4);
+
+        executor.submit(r3);
+        executor.submit(r4);
         
         // Shut down the thread pool once all tasks have completed
         executor.shutdown();
+    }
+
+    public static Runnable TaskTwo(int taskId) {
+        return ()->
+        {
+            System.out.println("TaskID: " + taskId + " is running.");
+        };
     }
 }
